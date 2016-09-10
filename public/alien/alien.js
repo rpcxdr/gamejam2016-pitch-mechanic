d16c20b30ccd817628073c38401120e4e3de0752
Vynlar <lightningzygella@gmail.com>
2016-09-10 17:40:24 -0600

    added round counting

--- public/alien/alien.js
+++ public/alien/alien.js
@@ -1,25 +1,34 @@
 "use strict";
 
-const client = deepstream('104.236.166.136:6020').login();
-const record = client.record.getRecord('room/1');
+const client = deepstream('104.236.166.136:6020', {
+    mergeStrategy: deepstream.MERGE_STRATEGIES.LOCAL_WINS
+}).login();
+const statusRecord = client.record.getRecord('room/1/status');
+const countRecord = client.record.getRecord('room/1/count');
 
 const statusField = document.getElementById('status');
 
-record.whenReady(function() {
-  statusField.innerHTML = record.get('alien-status');
+statusRecord.whenReady(function() {
+  statusField.innerHTML = statusRecord.get('alien-status');
 });
 
-record.subscribe('human-status', function(status) {
+statusRecord.subscribe('human-status', function(status) {
   if(status === 'ready') {
       // the human is now ready for you to play
       statusField.innerHTML = 'playing';
-      record.set('alien-status', 'playing');
+      statusRecord.set('alien-status', 'playing');
   } else {
       // the human started playing
   }
 });
 
 function done() {
-  record.set('alien-status', 'ready');
-  statusField.innerHTML = 'ready';
+  statusRecord.set('alien-status', 'ready');
+
+  let round = countRecord.get('round-count');
+  round++;
+  if(!round) countRecord.set('round-count', 1);
+  else countRecord.set('round-count', round);
+
+  statusField.innerHTML = 'ready ' + round;
 }
