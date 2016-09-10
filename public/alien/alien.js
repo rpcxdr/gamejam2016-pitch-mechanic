73717922b475be0f92b3b11e6c390ac27bdccadb
Vynlar <lightningzygella@gmail.com>
2016-09-10 13:36:32 -0600

    turn system works now

--- public/alien/alien.js
+++ public/alien/alien.js
@@ -1,9 +1,25 @@
 "use strict";
 
-const client = deepstream('104.236.166.136:6020').login({username: 'adrian'});
+const client = deepstream('104.236.166.136:6020').login();
 const record = client.record.getRecord('room/1');
 
+const statusField = document.getElementById('status');
+
 record.whenReady(function() {
-  const tones = record.get();
-  console.log(tones);
-})
+  statusField.innerHTML = record.get('alien-status');
+});
+
+record.subscribe('human-status', function(status) {
+  if(status === 'ready') {
+      // the human is now ready for you to play
+      statusField.innerHTML = 'playing';
+      record.set('alien-status', 'playing');
+  } else {
+      // the human started playing
+  }
+});
+
+function done() {
+  record.set('alien-status', 'ready');
+  statusField.innerHTML = 'ready';
+}
--- public/human/human.js
+++ public/human/human.js
@@ -1,6 +1,25 @@
 "use strict";
 
-const client = deepstream('104.236.166.136:6021').login();
+const client = deepstream('104.236.166.136:6020').login();
 const record = client.record.getRecord('room/1');
 
-record.set('tones', [330,440,660,880,990]);
+const statusField = document.getElementById('status');
+
+record.whenReady(function() {
+  statusField.innerHTML = record.get('human-status');
+});
+
+record.subscribe('alien-status', function(status) {
+  if(status === 'ready') {
+      // the alien is now ready
+      statusField.innerHTML = 'playing';
+      record.set('human-status', 'playing');
+  } else {
+      // the alien started playing
+  }
+});
+
+function done() {
+  record.set('human-status', 'ready');
+  statusField.innerHTML = 'ready';
+}
